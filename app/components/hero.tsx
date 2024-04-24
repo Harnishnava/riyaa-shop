d25a6770ee6851f1054a30ca5416caf9a4ffdd4b
Harnishnava <navaharnish5722@gmail.com>
2024-04-24 09:15:39 +0800

    update hero section

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,51 +1,52 @@
 import React from "react";
 import Image from "next/image";
-import HeroImage from "@/public/Remove-bg.ai_1711461625724.png";
+import HeroImage from "@/public/hero image3.jpg";
 
 const Hero = () => {
   return (
-    <section className="">
-      <div className="container mx-auto px-4 py-12 relative">
-        <div className="flex flex-col md:flex-row items-center justify-between relative z-10">
-          {/* Text on the Left */}
-          <div className="hidden md:flex-1 md:flex md:items-center justify-center text-center md:text-left text-sm">
-            <span className="bg-gray-300 rounded-full px-3 py-1">
-              "Discover"
-            </span>
-          </div>
-
-          {/* Image */}
-          <div className="flex-1 flex justify-center">
-            <div className="relative w-full h-full rounded-xl ">
-              <Image
-                src={HeroImage}
-                alt="Beauty Cream"
-                className="rounded-xl"
-                width={500}
-                height={500}
-              />
+    <div className="relative px-4 pt-16 mx-auto lg:py-32 md:px-8 xl:px-20 sm:max-w-xl md:max-w-full md:h-auto h-screen ">
+      <div className="max-w-xl mx-auto lg:max-w-screen-xl">
+        <div className="mb-16 lg:max-w-lg lg:mb-0">
+          <div className="max-w-xl mb-6">
+            <div>
+              <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+                Brand new
+              </p>
             </div>
-          </div>
-
-          {/* Text on the Right */}
-          <div className="flex-1 mt-8 md:mt-0 md:ml-12 md:items-end text-center md:text-left">
-            <h1 className="md:text-7xl text-3xl font-bold text-zinc-800">
-              Unlock Radiant Beauty
-            </h1>
-            <p className="mt-4 text-md text-zinc-600">
-              Get them together for less! For dewy, natural-looking coverage
-              that still looks like skin.
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              The quick, brown fox
+              <br className="hidden md:block" />
+              jumps over{" "}
+              <span className="inline-block text-deep-purple-accent-400">
+                a lazy dog
+              </span>
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae. explicabo.
             </p>
+          </div>
+          <div className="flex items-center">
             <a
-              href="#"
-              className="mt-6 inline-block bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 transition-colors"
+              href="/"
+              className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-black transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
             >
-              SHOP NOW
+              Get started
             </a>
           </div>
         </div>
       </div>
-    </section>
+      <div className="flex justify-center md:h-full md:overflow-y-clip  lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-end">
+        <Image
+          src={HeroImage}
+          className="object-cover object-top w-4/5 md:w-full md:h-auto min-w-52 -mb-16 rounded shadow-2xl lg:ml-64 xl:ml-8 lg:-mb-24 xl:-mb-28 lg:h-auto lg:max-w-screen-md"
+          height={1000}
+          width={1000}
+          alt=""
+        />
+      </div>
+    </div>
   );
 };
 
